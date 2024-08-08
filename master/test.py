ebf536d3a2700856b035008dbb0eb7102b9c2dd8
lxdtk <158218070@qq.com>
2024-08-08 18:05:22 +0800

    server

--- master/test.py
+++ master/test.py
@@ -1,1 +1,17 @@
-print("hello world")
+import flet as ft
+import flet.fastapi
+
+import uvicorn
+app = flet.fastapi.FastAPI()
+
+@app.get("/app")
+async def helloWorld():
+    return {"message": "ello app"}
+
+async def subMain(page:ft.Page):
+    await page.add_async(ft.Text("Hello subapp"))
+
+app.mount("/sub_main", flet.fastapi.app(subMain))
+
+if __name__ == "__main__":
+    uvicorn.run("test:app", host="0.0.0.0", port=8080, reload=True)
